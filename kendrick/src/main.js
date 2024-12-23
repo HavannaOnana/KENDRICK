d61ea20629b966c83d6fa0f7174b829c25e62523
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2024-12-23 23:53:06 +0100

    done with animations for the first part more too come

--- kendrick/src/main.js
+++ kendrick/src/main.js
@@ -1,47 +1,53 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import gsap from 'gsap';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+let Firstpart = document.querySelector('#firstpart')
+
+Firstpart.innerHTML = `
+    <div class="firstlayer">
+         <h2>negative.</h2>
+         <div class="navbar">
+           <a href="#">home</a>
+           <a href="#">about us </a>
+           <a href="#" class="playlist">playlist</a>
+         </div>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-document.querySelector("#firstpart").innerHTML = `
-  <div id="firstpart" class="firstpart">
-
-        <div class="firstlayer">
-             <h2>negative.</h2>
-             <div class="navbar">
-               <a href="#">home</a>
-               <a href="#">about us </a>
-               <a href="#" class="playlist">playlist</a>
-             </div>
-        </div>
-
-        <div class="firstparttext">
-            <h1>Kendrick<br>Lamar</h1>
-            <button type="button">explore</button>
-        </div>
-
-        <p class="info">Kendrick Lamar Duckworth is an American rapper, songwriter, and record producer.<br>
-           He is regarded as one of the most skillful and successfull hip hop artists<br> of his generation, with various media outlets<br> branding him the ``new king of hip hop'</p>
-        
+    <div class="firstparttext">
+        <h1>Kendrick<br>Lamar</h1>
+        <button type="button">explore</button>
     </div>
+
+    <p class="info">Kendrick Lamar Duckworth is an American rapper, songwriter, and record producer.<br>
+       He is regarded as one of the most skillful and successful hip hop artists<br> of his generation, with various media outlets<br> branding him the \`new king of hip hop\`</p>
 `;
 
-setupCounter(document.querySelector('#counter'))
+//adding GSAP animations
+document.addEventListener('DOMContentLoaded',()=>{
+  gsap.from(".firstparttext", {
+    duration: 4,
+    y: -50,
+    opacity: 0,
+    ease: "power1.out",
+  });
+
+  // the p text too
+  gsap.from(".info", {
+    duration: 4,
+    y: -50,
+    opacity: 0,
+    ease: "power1.out",
+  });
+
+   gsap.from(".firstlayer", {
+     duration: 4,
+     y: -50,
+     opacity: 0,
+     ease: "power1.out",
+   });
+
+})
+
+
+
+
